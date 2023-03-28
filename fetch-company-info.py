b621cfa9d059fc77b94411bc590f54aabe228f8d
Matt Drees <matt@drees.me>
2023-03-28 10:45:27 -0700

    Changed "company" to "ticker"

--- fetch-company-info.py
+++ fetch-company-info.py
@@ -7,30 +7,30 @@ import time
 from typing import List
 
 
-def fetch_weekly_company_prices(key: str, companies: List[str]):
+def fetch_weekly_company_prices(key: str, tickers: List[str]):
 
     # Get list of companies we already have.
     companies_already_have = [os.path.splitext(c)[0] for c in os.listdir("weekly")]
 
-    companies_to_fetch = set(companies) - set(companies_already_have)
-    companies_to_fetch = list(set(companies) - set(companies_already_have))
-    companies_to_fetch.sort()
+    tickers_to_fetch = set(tickers) - set(companies_already_have)
+    tickers_to_fetch = list(set(tickers) - set(companies_already_have))
+    tickers_to_fetch.sort()
     ALPHAVANTGE_MAX_QUERIES_PER_DAY = 500
-    companies_to_fetch = companies_to_fetch[:ALPHAVANTGE_MAX_QUERIES_PER_DAY]
-    for i, ticker in enumerate(companies_to_fetch):
+    tickers_to_fetch = tickers_to_fetch[:ALPHAVANTGE_MAX_QUERIES_PER_DAY]
+    for i, ticker in enumerate(tickers_to_fetch):
         print(f"{i}. {ticker}")
     print(
-        f"Companies total: {len(companies)}, already have: {len(companies_already_have)}, fetching: {len(companies_to_fetch)}"
+        f"Companies total: {len(tickers)}, already have: {len(companies_already_have)}, fetching: {len(tickers_to_fetch)}"
     )
 
     # Fetch weekly prices for each company
-    for company in companies_to_fetch:
-        print(company)
-        url = f"https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol={company}&apikey={key}"
+    for ticker in tickers_to_fetch:
+        print(ticker)
+        url = f"https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol={ticker}&apikey={key}"
         r = requests.get(url)
         if r.status_code != requests.codes.ok:
             r.raise_for_status()
-        with open(f"weekly/{company}.json", "w") as f:
+        with open(f"weekly/{ticker}.json", "w") as f:
             f.write(r.text)
         time.sleep(12)
 
@@ -40,8 +40,8 @@ if __name__ == "__main__":
     with open("config.json", "r") as f:
         key = json.load(f)["alphavantage-api-key"]
 
-    # Get list of sp500 companies.
+    # Get list of tickers.
     with open("healthcare-companies.txt", "r") as f:
-        companies = f.read().splitlines()
+        tickers = f.read().splitlines()
 
-    fetch_weekly_company_prices(key, companies)
+    fetch_weekly_company_prices(key, tickers)
